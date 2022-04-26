a3a8568e329d57663ad6fb319655ead9a1a6e317
Tricia S Amheiser <tricia@taarts.net>
2022-04-25 23:28:15 -0400

    while, for, foreach

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Loops
 {
@@ -6,9 +7,80 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to C#");
+            // repeated task x# || until a condition is met
+            // process all of the items in a collection
+
+            var counter = 0;
+
+            while (counter < 5) // these stmts do not require a ";" outside {}
+            {
+                Console.WriteLine("Doughnuts");
+
+                counter++;
+            }
+            // for LOOP
+            /* has 3 parts 
+            for(INITIALIZATION; CONDITION; AFTERTHOUGHT){
+              //Loop stmts
+            "for" Loop is a compact "while" Loop
+            } */
+            // INITIALIZER
+            for (var forCounter = 0; forCounter < 4; forCounter++)
+            {
+                Console.WriteLine("Doughnuts again");
+            }
+
+            var names = new List<string>() { "Gaga", "Bey", "Harry" };
+
+            names.Add("Mel");
+            names.Add("Baby");
+            names.Add("Posh");
+
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            names.Add(name);
+
+            while (name != "quit")
+            {
+                Console.WriteLine($"  -----{names.Count} people -----");
+                //print out whole list 
+                // for (var index = 0; index < names.Count; index++)
+                // {
+
+                //     var currentName = names[index];
+                //     Console.WriteLine(currentName);
+                // }
+
+                /* <--------FOREACH */
+                // foreach (var currentName in names)
+                // {
+                //     Console.WriteLine(currentName);
+                // }
+
+                // names.Reverse();
+                // foreach (var currentName in names)
+                // {
+                //     Console.WriteLine(currentName);
+                // }
+
+                for (var index = names.Count - 1; index >= 0; index--)
+                {
+
+                    var currentName = names[index];
+                    Console.WriteLine($"The person at index {index} is {currentName}");
+
+                }
+                Console.WriteLine();
+
+                Console.Write("What is your name? ");
+                name = Console.ReadLine();
+                names.Add(name);
+            }
+            //Pretending we are .NET
+
         }
 
 
     }
 }
+
